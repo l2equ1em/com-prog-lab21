509447c87ca9147e3302d4eb034e247b37d3635f
LAB303-24 <lab303-24@vdieng.local>
2025-02-18 15:06:30 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc, char* argv[]){
+    double sum = 0;
+    for(int i = 0; i < argc; i++){
+        sum += atof(argv[i]);
+    }
+    if(argc > 1){
+        cout << "---------------------------------" << endl;
+        cout << "Average of " <<  argc - 1 << " numbers = " << (sum/(argc-1)) << endl;
+        cout << "---------------------------------";
+    }
+    else cout << "Please input numbers to find average.";
+}
